477c543e049a124ed5be827a49ed8a51c82f2c83
Mys9ys9ka <rjbexa@yandex.ru>
2022-09-16 14:47:24 +0300

    v 1.0154 допиливаем стили

---
+++ pkb_monitor.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/script/monitor.css">
+</head>
+<body>
+<div class="pkb-monitor">
+    <p>Отключить: <input type="checkbox" id="PKBDisable"></p>
+    <p>Трофеи: <b>45</b></p>
+    <p>До обновления заданий: <b>7:55</b></p>
+    <p>До приема пищи: <b>2:12</b></p></div>
+</body>
+</html>
+
