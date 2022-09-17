d9f4243057d889248e7b0419fe6d709855dbf8eb
Mys9ys9ka <rjbexa@yandex.ru>
2022-09-17 16:37:31 +0300

    v 1.1.525 cors

--- parse.php
+++ parse.php
@@ -1,7 +1,10 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
+
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/core/internal_settings.php';
 
 $id = 747762770;
