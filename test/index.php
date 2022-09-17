cf678f200c41027d98f0b943474815fa8fcf53b3
Mys9ys9ka <rjbexa@yandex.ru>
2022-09-17 17:01:38 +0300

    v 1.1.5.33 cors

---
+++ test/index.php
@@ -0,0 +1,23 @@
+<?php
+header('Access-Control-Allow-Origin: https://vk.com');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+header('Content-Type: text/html; charset=utf-8');
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/test/internal_settings.php';
+
+$id = 510815492;
+
+$mes = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/upload/'.$id.'mes.json');
+
+$test = new \test\ParseMessage();
+
+echo '<pre>';
+var_dump(json_decode($mes));
+var_dump($test->firstParseMessage($mes));
+echo '</pre>';
