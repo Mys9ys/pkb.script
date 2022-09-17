c9f25186df728e0890debf0f070f84fd9a7cb933
Mys9ys9ka <rjbexa@yandex.ru>
2022-09-17 16:07:41 +0300

    v 1.1.513 cors

--- ajax/index.php
+++ ajax/index.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: https://vk.com');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/core/internal_settings.php';
+//require_once $_SERVER['DOCUMENT_ROOT'] . '/core/internal_settings.php';
 
 if ($_REQUEST) {
 
@@ -16,7 +16,10 @@ if ($_REQUEST) {
     fwrite($fp, $send);
     fclose($fp);
 
-    $test = new \core\ParseMessage();
+//    $test = new \core\ParseMessage();
+//
+//    echo json_encode($test->firstParseMessage($send));
 
-    echo json_encode($test->firstParseMessage($send));
 }
+
+echo 'test';
